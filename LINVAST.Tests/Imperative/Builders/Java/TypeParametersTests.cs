c3b86f64597541c115bf1d30e4394c02fc9697a2
Dara Milojkovic <milojkovic.dara@gmail.com>
2021-02-18 22:31:17 +0100

    Types and type tests implemented

---
+++ LINVAST.Tests/Imperative/Builders/Java/TypeParametersTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINVAST.Imperative.Builders.Java;
+using LINVAST.Imperative.Nodes;
+using LINVAST.Nodes;
+using NUnit.Framework;
+
+namespace LINVAST.Tests.Imperative.Builders.Java
+{
+    internal sealed class TypeParametersTests : ASTBuilderTestBase
+    {
+
+        [Test]
+
+        public void TypeParameterTest()
+        {
+            TypeNameListNode ast = this.GenerateAST("< A >").As<TypeNameListNode>();
+
+            Assert.That(ast.Types.First().Identifier, Is.EqualTo("A"));
+        }
+
+        [Test]
+
+        public void TypeParametersTest()
+        {
+            TypeNameListNode ast = this.GenerateAST("< A, B >").As<TypeNameListNode>();
+
+            Assert.That(ast.Types.First().Identifier, Is.EqualTo("A"));
+            Assert.That(ast.Types.Last().Identifier, Is.EqualTo("B"));
+        }
+
+        [Test]
+        public void TypeParameterWithExtendsTest()
+        {
+
+            TypeNameListNode ast = this.GenerateAST("< A extends B>").As<TypeNameListNode>();
+
+            Assert.That(ast.Types.First().Identifier, Is.EqualTo("A"));
+            Assert.That(ast.Types.First().BaseTypes.Identifiers.First().Identifier, Is.EqualTo("B"));
+        }
+        [Test]
+        public void TypeParametersWithExtendsTest()
+        {
+
+            TypeNameListNode ast = this.GenerateAST("< A extends C, B>").As<TypeNameListNode>();
+
+            Assert.That(ast.Types.First().Identifier, Is.EqualTo("A"));
+            Assert.That(ast.Types.First().BaseTypes.Identifiers.First().Identifier, Is.EqualTo("C"));
+            Assert.That(ast.Types.Last().Identifier, Is.EqualTo("B"));
+        }
+
+        [Test]
+        public void TypeParametersWithExtendsMultipleTest()
+        {
+
+            TypeNameListNode ast = this.GenerateAST("< A extends C & D, B>").As<TypeNameListNode>();
+
+            Assert.That(ast.Types.First().Identifier, Is.EqualTo("A"));
+            Assert.That(ast.Types.First().BaseTypes.Identifiers.First().Identifier, Is.EqualTo("C"));
+            Assert.That(ast.Types.First().BaseTypes.Identifiers.Last().Identifier, Is.EqualTo("D"));
+            Assert.That(ast.Types.Last().Identifier, Is.EqualTo("B"));
+        }
+        protected override ASTNode GenerateAST(string src)
+        {
+            return new JavaASTBuilder().BuildFromSource(src, p => p.typeParameters());
+
+        }
+
+    }
+}
---
+++ LINVAST.Tests/Imperative/Builders/Java/TypeTypeTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINVAST.Imperative.Builders.Java;
+using LINVAST.Imperative.Nodes;
+using LINVAST.Nodes;
+using NUnit.Framework;
+
+namespace LINVAST.Tests.Imperative.Builders.Java
+{
+    internal sealed class TypeTypesTests : ASTBuilderTestBase
+    {
+        [Test]
+
+        public void PrimitiveTypeTest()
+        {
+
+            TypeNameNode ast = this.GenerateAST("int").As<TypeNameNode>();
+
+
+            Assert.That(ast.Identifier, Is.EqualTo("int"));
+        }
+
+        [Test]
+        public void VoidTypeTest()
+        {
+            TypeNameNode ast = this.GenerateAST("void").As<TypeNameNode>();
+
+            Assert.That(ast.Identifier, Is.EqualTo("void"));
+        }
+
+        [Test]
+
+        public void ClassOrInterfaceTypeTest()
+        {
+
+            TypeDeclNode ast = this.GenerateAST("A").As<TypeDeclNode>();
+
+            Assert.That(ast.Identifier, Is.EqualTo("A"));
+        }
+
+        [Test]
+        public void ClassOrInterfaceTypeWithTypeArgumentTest()
+        {
+
+            TypeDeclNode ast = this.GenerateAST("A<B>").As<TypeDeclNode>();
+
+            Assert.That(ast.Identifier, Is.EqualTo("A"));
+            Assert.That(ast.TemplateParameters.Identifiers.First().Identifier, Is.EqualTo("B"));
+        }
+
+        [Test]
+        public void ClassOrInterfaceTypeWithTypeArgumentsTest()
+        {
+
+            TypeDeclNode ast = this.GenerateAST("A<B, C>").As<TypeDeclNode>();
+
+            Assert.That(ast.Identifier, Is.EqualTo("A"));
+            Assert.That(ast.TemplateParameters.Identifiers.First().Identifier, Is.EqualTo("B"));
+            Assert.That(ast.TemplateParameters.Identifiers.Last().Identifier, Is.EqualTo("C"));
+        }
+
+        protected override ASTNode GenerateAST(string src)
+        {
+            return new JavaASTBuilder().BuildFromSource(src, p => p.typeTypeOrVoid());
+        }
+    }
+}
