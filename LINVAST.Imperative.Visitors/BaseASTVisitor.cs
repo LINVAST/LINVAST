da3267ba355b5e5dd620195461d9d31e78a2df96
Svetozar Ikovic <ikovic.svetozar@gmail.com>
2026-06-21 20:44:34 +0200

    Add global and nonlocal stat nodes

--- LINVAST.Imperative.Visitors/BaseASTVisitor.cs
+++ LINVAST.Imperative.Visitors/BaseASTVisitor.cs
@@ -62,6 +62,7 @@ namespace LINVAST.Imperative.Visitors
         public virtual TResult Visit(FuncParamsNode node) => this.VisitChildren(node);
         public virtual TResult Visit(IdNode node) => this.VisitChildren(node);
         public virtual TResult Visit(IdListNode node) => this.VisitChildren(node);
+        public virtual TResult Visit(GlobalStatNode node) => this.VisitChildren(node);
         public virtual TResult Visit(IfStatNode node) => this.VisitChildren(node);
         public virtual TResult Visit(IncExprNode node) => this.VisitChildren(node);
         public virtual TResult Visit(JumpStatNode node) => this.VisitChildren(node);
@@ -70,6 +71,7 @@ namespace LINVAST.Imperative.Visitors
         public virtual TResult Visit(LitExprNode node) => this.VisitChildren(node);
         public virtual TResult Visit(LogicExprNode node) => this.VisitChildren(node);
         public virtual TResult Visit(MatchStatNode node) => this.VisitChildren(node);
+        public virtual TResult Visit(NonlocalStatNode node) => this.VisitChildren(node);
         public virtual TResult Visit(NullLitExprNode node) => this.VisitChildren(node);
         // Python-specific patterns
         public virtual TResult Visit(PatternAsNode node) => this.VisitChildren(node);
