9b71d0d9cd5ef8f0909ce596dc81609cf055aad6
mi16087 <mihajlozivkovic97@gmail.com>
2026-06-22 18:39:17 +0200

    Add Python with statement support

--- LINVAST.Imperative.Visitors/BaseASTVisitor.cs
+++ LINVAST.Imperative.Visitors/BaseASTVisitor.cs
@@ -75,6 +75,7 @@ namespace LINVAST.Imperative.Visitors
         public virtual TResult Visit(UnaryOpNode node) => this.VisitChildren(node);
         public virtual TResult Visit(VarDeclNode node) => this.VisitChildren(node);
         public virtual TResult Visit(WhileStatNode node) => this.VisitChildren(node);
+        public virtual TResult Visit(WithStatNode node) => this.VisitChildren(node);
         public virtual TResult Visit(YieldExprNode node) => this.VisitChildren(node);
 
         protected virtual TResult DefaultResult => default!;
